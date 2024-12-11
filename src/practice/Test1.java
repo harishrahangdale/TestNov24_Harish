a6914a412d2ad5b6023b620658a3c8ed4af28ca4
Harish Rahangdale <hsr.29978@gmail.com>
2024-12-11 08:26:27 +0530

    Upload test project

---
+++ src/practice/Test1.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is test class 1");
+	}
+
+}
---
+++ src/practice/Test2.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is test class 2");
+	}
+
+}
