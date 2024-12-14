934aaac80b14268e808fd6e885fd8f52152e4007
Harish Rahangdale <hsr.29978@gmail.com>
2024-12-14 12:05:05 +0530

    branch b changes for second conflict scenario

--- src/practice/Test1.java
+++ src/practice/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello_class1, Branch A changes, second add Branch A");
-		System.out.println("Branch B changes, second changes in Branch B");
+		System.out.println("Branch B changes, conflict second scenario");
 	}
 
 }
